509cacaeb34dff76df5c850535b3b626f6d0ecc2
llammann <bd7n2v9tr@code.edu.az>
2023-10-12 19:48:03 +0400

    loops

---
+++ js/lo1/app.js
@@ -0,0 +1,97 @@
+loop;
+// #region TASK 1
+
+// let input=Number(window.prompt("Enter count"))
+// for(let i= 1; i<=input; i++) {
+//     console.log("Hello World")
+// }
+
+// #endregion
+
+// #region TASK 2
+
+// let input = Number(window.prompt("Enter number"));
+// let factorial = 1;
+// for (let i = 1; i <= input; i++) {
+//   factorial *= i;
+// }
+// window.alert(`factorial: ${factorial}`);
+
+// #endregion TASK 2
+
+// #region TASK 3
+
+// let number_1 = Number(window.prompt("Enter  first number "));
+// let number_2;
+// let sum = 0;
+// do {
+//   number_2 = Number(window.prompt("Enter second number "));
+//   if (number_1 < number_2) {
+//     for (i = number_1; i < number_2; i++) sum += i;
+//   }
+// } while (number_2 < number_1);
+
+// window.alert(`${sum}`);
+
+// #endregion
+
+// #region TASK 4
+
+// let input = window.prompt("Enter number");
+// console.log(input.length);
+
+// #endregion
+
+// #region TASK 5
+
+// let input = String(window.prompt("Enter string"));
+// let count = 1;
+// for (let i = 1; i < input.length; i++) {
+//   if (input[i] == ",") {
+//     count++;
+//   }
+// }
+// console.log(`${count}`);
+
+// #endregion
+
+// #region TASK 6
+
+// let number = window.prompt("Enter number");
+// let pow = window.prompt("Enter pow");
+// window.alert(`result: ${Math.pow(number, pow)}`);
+
+// #endregion
+
+// arr
+// #region TASK 1
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// let result = sum / arr.length;
+// console.log(result);
+
+// #endregion
+
+// #region TASK 2
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let max = arr[0];
+// let min = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+// }
+// let result = (max + min) / 2;
+// console.log(result);
+
+// #endregion
+
+// #region TASK 3
