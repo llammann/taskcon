0a7833b2d96cb7f5df6cdcd52f01ac122da857ff
llammann <bd7n2v9tr@code.edu.az>
2023-10-31 20:05:25 +0400

    hjsg

---
+++ week_7/singer.wishlist/app.js
@@ -0,0 +1,30 @@
+sin_url = "http://localhost:3000/singers";
+let cards = document.querySelector(".cards");
+axios(sin_url).then((res) => {
+  let datas = res.data;
+  datas.forEach((singer) => {
+    cards.innerHTML += `
+    <div class="col-4">
+    <div class="my_card card" style="width: 18rem;">
+    <div class="img_wrapper"><img src="${singer.image}" class="my_card_img card-img-top" alt="..."></div>
+    <div class="my_card_body card-body">
+        <h5 class="my_card_name card-title">${singer.name}</h5>
+        <p class="my_card_text card-text"><span class="name">${singer.name}</span> is <span
+                class="nationality">${singer.nationality}</span></p>
+        <button class="trash_btn p-3 btn btn-secondary"><i class="fa-solid fa-trash fa-2x"
+                style="color: #c92626;"></i></button>
+        <button name="${singer.id}" class="fav_btn  p-3 btn btn-secondary"><i class="fa-solid fa-heart fa-2x"
+                style="color: #b82828;"></i></button>
+
+    </div>
+</div>
+</div>
+`;
+    //     // let fav_btns = document.querySelectorAll(".fav_btn");
+    // fav_btns.forEach((fav_btn) => {
+    //   fav_btn.addEventListener("click", function () {});
+    // });
+
+    // WISHLIST
+  });
+});
